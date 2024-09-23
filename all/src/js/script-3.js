9ab4377cd909458270f017d5033108fa6808a42a
develop-ale <144731847+develop-ale@users.noreply.github.com>
2024-09-23 22:03:05 +0200

    cfj

--- all/src/js/script-3.js
+++ all/src/js/script-3.js
@@ -1,13 +1,13 @@
 function scorrimentoTitolo() {
     let titolo = "Questo è il titolo della mia pagina che scorre! ";
     let index = 0;
+    console.log("Inizio scorrimento titolo");
 
-    // Funzione che aggiorna il titolo progressivamente
     setInterval(function() {
+        console.log("Aggiornamento titolo", titolo.substring(index) + titolo.substring(0, index)); // Aggiungi log
         document.title = titolo.substring(index) + titolo.substring(0, index);
         index = (index + 1) % titolo.length;
-    }, 200); // Cambia ogni 200 millisecondi (modificabile)
+    }, 200);
 }
 
-// Avvio dello scorrimento al caricamento della pagina
 window.onload = scorrimentoTitolo;
