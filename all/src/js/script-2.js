2c08f2855983c318eb0d62a13346a41760252159
develop-ale <144731847+develop-ale@users.noreply.github.com>
2024-09-23 18:06:55 +0200

    dtyj

--- all/src/js/script-2.js
+++ all/src/js/script-2.js
@@ -1,7 +1,9 @@
-// Controlla se l'utente ha già risposto al consenso sui cookie
 window.onload = function () {
     if (!getCookie("cookieConsent")) {
         document.getElementById("cookiePopup").style.display = "flex";
+        document.getElementById("cookieOverlay").style.display = "block";
+    } else {
+        enablePage();
     }
 };
 
@@ -30,14 +32,22 @@ function getCookie(cname) {
     return "";
 }
 
+// Abilita l'interazione con la pagina
+function enablePage() {
+    document.querySelector('.all').style.pointerEvents = 'all';
+    document.querySelector('.all').style.opacity = '1';
+    document.getElementById("cookiePopup").style.display = "none";
+    document.getElementById("cookieOverlay").style.display = "none";
+}
+
 // Azione al click del pulsante "Accetta tutti"
 document.getElementById("acceptCookies").onclick = function () {
     setCookie("cookieConsent", "accepted", 365);
-    document.getElementById("cookiePopup").style.display = "none";
+    enablePage();
 };
 
 // Azione al click del pulsante "Rifiuta tutti"
 document.getElementById("rejectCookies").onclick = function () {
     setCookie("cookieConsent", "rejected", 365);
-    document.getElementById("cookiePopup").style.display = "none";
+    enablePage();
 };
